6bff00a333db58834cb4ac639e1e81339c3f26d2
Michael Voznesensky <voznesenskym@gmail.com>
2022-12-01 08:31:24 +0000

    [WIP] Commit to run CI on vaguely sus C++ changes uncovered during fixing upstream prototype
    ghstack-source-id: 93ea05c17dee12c63ac6128bb1524b5c8fb13fee
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/89773
    
    Wip
    
    Random stuff
    
    Fix tests
    
    Fix char
    
    make shape prop fake tensor friendly
    
    Fix
    
    Test fixes
    
    Feedback, test fixes, xla shenanigins
    
    xla maddness
    
    xla maddness
    
    Feedback

--- test/dynamo/test_minifier.py
+++ test/dynamo/test_minifier.py
@@ -113,7 +113,9 @@ class MinifierTests(MinifierTestBase):
         )
 
     def test_after_dynamo_cpu_accuracy_error(self):
-        self._test_after_dynamo("cpu", 4, RELU_ACCURACY_ERROR_BACKEND, "AccuracyError")
+        self._test_after_dynamo(
+            "cpu", 4, RELU_ACCURACY_ERROR_BACKEND, "Accuracy failed"
+        )
 
     @requires_cuda()
     def test_after_dynamo_cuda_compile_error(self):
@@ -129,7 +131,9 @@ class MinifierTests(MinifierTestBase):
 
     @requires_cuda()
     def test_after_dynamo_cuda_accuracy_error(self):
-        self._test_after_dynamo("cuda", 4, RELU_ACCURACY_ERROR_BACKEND, "AccuracyError")
+        self._test_after_dynamo(
+            "cuda", 4, RELU_ACCURACY_ERROR_BACKEND, "Accuracy failed"
+        )
 
     # Ensure that the testing backends pass when relu is not present.
     def _test_after_dynamo_backend_passes(self, device, repro_level, backend_code):
@@ -193,11 +197,15 @@ class MinifierTests(MinifierTestBase):
         """
         )
 
+        repro_after = "dynamo"
+        repro_level = 2
         test_code = self._gen_test_code(
-            run_code, "dynamo", 2, RELU_CUSTOM_ERROR_BACKEND, False
+            run_code, repro_after, repro_level, RELU_CUSTOM_ERROR_BACKEND, False
         )
         _, repro_dir = self._run_test_code(test_code)
-        launch_proc, _ = self._run_minifier_launcher("", repro_dir)
+        launch_proc, _ = self._run_minifier_launcher(
+            "", repro_dir, repro_after, repro_level
+        )
         self.assertIn("RuntimeError", launch_proc.stderr.decode("utf-8"))
 
     # Test that a module with mixed cpu/cuda parts with an error after dynamo can be repro'd
